c1a3cc73aa1b8c3a57a78d1a4b31baf3f05b8893
drew <85300755+dahroo@users.noreply.github.com>
2025-07-16 22:07:53 -0700

    Initial Commit

---
+++ Lifetimer/LifetimerApp.swift
@@ -0,0 +1,17 @@
+//
+//  LifetimerApp.swift
+//  Lifetimer
+//
+//  Created by Andrew Zhou on 7/16/25.
+//
+
+import SwiftUI
+
+@main
+struct LifetimerApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
---
+++ LifetimerTests/LifetimerTests.swift
@@ -0,0 +1,17 @@
+//
+//  LifetimerTests.swift
+//  LifetimerTests
+//
+//  Created by Andrew Zhou on 7/16/25.
+//
+
+import Testing
+@testable import Lifetimer
+
+struct LifetimerTests {
+
+    @Test func example() async throws {
+        // Write your test here and use APIs like `#expect(...)` to check expected conditions.
+    }
+
+}
